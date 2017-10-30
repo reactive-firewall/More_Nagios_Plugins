11be84bcdfab2b2fd467d0f024c007e223036634
reactive-firewall <reactive-firewall@users.noreply.github.com>
2017-10-30 14:08:54 -0700

    moved some code to functions

--- check_arp.py
+++ check_arp.py
@@ -1,17 +1,23 @@
 #! /usr/bin/env python
 
+
 # default values
-DEFAULT_MAC_ADDR='ff:ff:ff:ff:ff:ff'
+DEFAULT_MAC_ADDR = str("""ff:ff:ff:ff:ff:ff""")
+"""The Default MAC address to check is all one bits, i.e. broadcast address."""
 
 import argparse
-# add to function
-parser = argparse.ArgumentParser(description='check for an arp entry')
-parser.add_argument('-M', '--mac', default=DEFAULT_MAC_ADDR, help='check for an exact MAC')
-parser.add_argument('-H', '--host', required=True, help='check for an exact IP')
-group_empty = parser.add_mutually_exclusive_group()
-group_empty.add_argument('-C', '--critical', default=False, action='store_true', help='return critical instead of warning on empty MAC, overides other empty values')
-group_empty.add_argument('-U', '--empty-unknown', default=False, action='store_true', help='return unknown instead of warning on empty MAC')
-group_empty.add_argument('--empty-ok', default=False, action='store_true', help='return ok on empty MAC instead, cannot be combined with optionals')
+
+
+def parseArgs(arguments=None):
+	parser = argparse.ArgumentParser(description='check for an arp entry')
+	parser.add_argument('-M', '--mac', default=DEFAULT_MAC_ADDR, help='check for an exact MAC')
+	parser.add_argument('-H', '--host', required=True, help='check for an exact IP')
+	group_empty = parser.add_mutually_exclusive_group()
+	group_empty.add_argument('-C', '--critical', default=False, action='store_true', help='return critical instead of warning on empty MAC, overides other empty values')
+	group_empty.add_argument('-U', '--empty-unknown', default=False, action='store_true', help='return unknown instead of warning on empty MAC')
+	group_empty.add_argument('--empty-ok', default=False, action='store_true', help='return ok on empty MAC instead, cannot be combined with optionals')
+	return parser.parse_args(arguments)
+
 
 def readFile(somefile):
 	import os
@@ -22,6 +28,7 @@ def readFile(somefile):
 	f.close()
 	return read_data
 
+
 def extractRegexPattern(theInput_Str, theInputPattern):
 	import re
 	sourceStr = str(theInput_Str)
@@ -29,8 +36,10 @@ def extractRegexPattern(theInput_Str, theInputPattern):
 	theList = prog.findall(sourceStr)
 	return theList
 
+
 def extractMACAddr(theInputStr):
 	return extractRegexPattern(theInputStr, "(?:(?:[[:print:]]*){0,1}(?P<Mac>(?:(?:[0-9a-fA-F]{1,2}[\:]{1}){5}(?:[0-9a-fA-F]{1,2}){1}){1})+(?:[[:print:]]*){0,1})+")
+
 
 def compactList(list, intern_func=None):
    if intern_func is None:
@@ -44,39 +53,23 @@ def compactList(list, intern_func=None):
        result.append(item)
    return result
 
-# make main function	
-args = parser.parse_args()
-test_MAC = (args.mac).lower()
-if (test_MAC is None):
-	print "check_arp: SYNTAX ERROR: MAC can not be set to None!"
-	exit(3)
 
-test_IP = args.host
-test_is_critical = args.critical
-test_is_unknown = args.empty_unknown
-test_is_inverted = args.empty_ok
-
-if test_IP is not None:
-	import subprocess
-	theResult=subprocess.check_output(["arp", "-n", test_IP])
-	if theResult is not None:
-		theMAC = extractMACAddr(theResult)
-		if (theMAC is None) and (len(theMAC) is 0):
-			# too complex move to function
-			if test_is_critical is True:
-				print "ARP CRITICAL - "+str(theMAC)
-				exit(2)
-			if test_is_unknown is True:
-				print "ARP UNKNOWN - "+str(theMAC)
-				exit(3)
-			if test_is_inverted is True:
-				print "ARP OK - "+str(theMAC)
-				exit(0)
-			else:
-				print "ARP WARNING - "+str(theMAC)
-				exit(1)
-		elif (theMAC is not None) and (len(theMAC) > 0) and (theMAC[0] is not None):
-			if (test_MAC != DEFAULT_MAC_ADDR) and (len(test_MAC) > 0) and (str(theMAC[0]).lower() != test_MAC):
+def main(argv=[]):
+	args = parseArgs(argv)
+	test_MAC = (args.mac).lower()
+	if (test_MAC is None):
+		print("check_arp: SYNTAX ERROR: MAC can not be set to None!")
+		exit(3)
+	test_IP = args.host
+	test_is_critical = args.critical
+	test_is_unknown = args.empty_unknown
+	test_is_inverted = args.empty_ok
+	if test_IP is not None:
+		import subprocess
+		theResult=subprocess.check_output(["arp", "-n", test_IP])
+		if theResult is not None:
+			theMAC = extractMACAddr(theResult)
+			if (theMAC is None) and (len(theMAC) is 0):
 				# too complex move to function
 				if test_is_critical is True:
 					print "ARP CRITICAL - "+str(theMAC)
@@ -89,24 +82,45 @@ if test_IP is not None:
 					exit(0)
 				else:
 					print "ARP WARNING - "+str(theMAC)
-					exit(1)
-			else:
-				print "ARP OK - "+str(theMAC[0])
-		else:
-			# too complex move to function
-			if test_is_critical is True:
-				print "ARP CRITICAL - EMPTY"
-				exit(2)
-			if test_is_unknown is True:
-				print "ARP UNKNOWN"
-				exit(3)
-			if test_is_inverted is True:
-				print "ARP OK - EMPTY"
-				exit(0)
-			else:
-				print "ARP WARNING - EMPTY"
 				exit(1)
-	else:
-		print "ARP UNKNOWN"
-		exit(3)
-exit(0)
+			elif (theMAC is not None) and (len(theMAC) > 0) and (theMAC[0] is not None):
+				if (test_MAC != DEFAULT_MAC_ADDR) and (len(test_MAC) > 0) and (str(theMAC[0]).lower() != test_MAC):
+					# too complex move to function
+					if test_is_critical is True:
+						print "ARP CRITICAL - "+str(theMAC)
+						exit(2)
+					if test_is_unknown is True:
+						print "ARP UNKNOWN - "+str(theMAC)
+						exit(3)
+					if test_is_inverted is True:
+						print "ARP OK - "+str(theMAC)
+						exit(0)
+					else:
+						print "ARP WARNING - "+str(theMAC)
+						exit(1)
+				else:
+					print "ARP OK - "+str(theMAC[0])
+			else:
+				# too complex move to function
+				if test_is_critical is True:
+					print "ARP CRITICAL - EMPTY"
+					exit(2)
+				if test_is_unknown is True:
+					print "ARP UNKNOWN"
+					exit(3)
+				if test_is_inverted is True:
+					print "ARP OK - EMPTY"
+					exit(0)
+				else:
+					print "ARP WARNING - EMPTY"
+					exit(1)
+		else:
+			print "ARP UNKNOWN"
+			exit(3)
+	exit(0)
+
+
+if __name__ in '__main__':
+	import sys
+	main(sys.argv[1:])
+
